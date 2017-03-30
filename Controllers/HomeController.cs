0933fb329b3f82452b371804bd832383fabeff4a
MikeGuimond <mhguimond@hotmail.com>
2017-03-30 15:15:29 -0400

    comments added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,14 +8,22 @@ namespace coreMvcTest.Controllers
     {
         public IActionResult Index()
         {
+            //These are the header keys that we would be interested in. 
+            // 1) SM_USER and USER_UID both return the login name, guimondmi for me or strongsr for you.
+            // 2) NIH_EMPLOYEEID will return the NED ID of the authenticated person.
+            // 3) you can also use USER_DN to display the users "Display name" i.e. Strong, Stephen (NIH\OD) [C]
+            string strHeaderKey =  HttpContext.Request.Headers["User-Agent"].ToString();
+
+            HttpContext.Session.SetString("headerString", strHeaderKey);
+
             return View();
         }
 
         public IActionResult About()
         {
-            string strHeaderKey =  HttpContext.Request.Headers["User-Agent"].ToString();
+            string strSessionValue =  HttpContext.Session.GetString("headerString");
 
-            ViewBag.Message = String.Format("User-Agent: {0}", strHeaderKey);
+            ViewBag.Message = String.Format("User-Agent: {0}", strSessionValue);
 
             return View();
         }
