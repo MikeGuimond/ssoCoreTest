460d8ab660fa0b3ffb48d624ee3b1bdb64592fc8
Guimond <guimondmi@nih.gov>
2017-03-30 09:56:50 -0400

    First mass commit to Git

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace coreMvcTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var headers = String.Empty;
+            foreach (var key in HttpContext.Request.Headers.Keys)
+                ViewBag.Message = headers += key + "=" + HttpContext.Request.Headers[key] + Environment.NewLine;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
